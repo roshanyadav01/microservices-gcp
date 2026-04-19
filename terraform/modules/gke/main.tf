433c01ea8a2f7229ad47b636cfc2fbac8dd5eacf
roshanyadav01 <roshan1992yadav@gmail.com>
2026-04-20 00:17:21 +0530

    Next Run

--- terraform/modules/gke/main.tf
+++ terraform/modules/gke/main.tf
@@ -1,6 +1,10 @@
+############################################
+# GKE CLUSTER
+############################################
+
 resource "google_container_cluster" "gke" {
   name     = "microservices-cluster"
-  location = var.zone
+  location = var.zone   # e.g. us-central1-a
 
   remove_default_node_pool = true
   initial_node_count       = 1
@@ -8,32 +12,51 @@ resource "google_container_cluster" "gke" {
   network    = var.network
   subnetwork = var.subnetwork
 
+  # VPC-native cluster
   ip_allocation_policy {}
 
   lifecycle {
     prevent_destroy = true
+
+    # Ignore fields that commonly drift
     ignore_changes = [
-      node_config,
-      initial_node_count,
+      initial_node_count
     ]
   }
-
 }
+
+############################################
+# NODE POOL (COST OPTIMIZED)
+############################################
 
 resource "google_container_node_pool" "cheap_pool" {
   name     = "cheap-pool"
-  cluster  = google_container_cluster.gke.name
+  cluster  = google_container_cluster.gke.id
   location = var.zone
 
-  node_count = 1
+  # Autoscaling instead of fixed nodes
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 2
+  }
 
   node_config {
     machine_type = "e2-small"
     disk_size_gb = 30
-    preemptible  = true
+
+    # Cheap but interruptible
+    preemptible = true
 
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
+    ]
+
+    tags = ["gke-node"]
+  }
+
+  lifecycle {
+    ignore_changes = [
+      node_count
     ]
   }
 }
